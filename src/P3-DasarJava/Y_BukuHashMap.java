740d1e113278b58a8a25b1ed846111066f6916f6
PC 07 <unch3000@gmail.com>
2022-03-28 16:47:31 +0700

    Dasar Java

--- src/P3-DasarJava/Y_BukuHashMap.java
+++ src/P3-DasarJava/Y_BukuHashMap.java
@@ -1,13 +1,13 @@
-/*import java.util.HashMap;
+import java.util.HashMap;
 import java.util.Map;
 public class Y_BukuHashMap {
     public static void main(String[] args) {
         //membuat objek hashmap
-        HashMap<String, Buku>books = new HashMap<>();
+        HashMap<String, X_Buku>books = new HashMap<>();
         //membuat objek buku
-        Buku bukuJava=new Buku("Tutorial java", "petani kode");
-        Buku bukuKotlin=new Buku("Pemrogramana kotlin","Petani Kode");
-        Buku bukuAndroid=new Buku("Pemrogramana Android", "Petani Kode");
+        X_Buku bukuJava = new X_Buku("Tutorial java", "Petani Kode");
+        X_Buku bukuKotlin = new X_Buku("Pemrograman kotlin","Petani Kode");
+        X_Buku bukuAndroid = new X_Buku("Pemrograman Android", "Petani Kode");
         //mengisi objek hashmap dengan objek buku
         books.put("java",bukuJava);
         books.put("kotlin",bukuKotlin);
@@ -15,12 +15,17 @@ public class Y_BukuHashMap {
         //print keys
         System.out.println("\nDaftar key: ");
         for(String i:books.keySet()){
-            System.out.println("Title: "+aBook.getTitle()+", Author: "+aBook.getAuthor());
+            System.out.println(i);
+        }
+        //print value
+        System.out.println("\nDaftar value: ");
+        for(X_Buku aBook:books.values()) {
+            System.out.println("Title: " + aBook.getTitle() + ", Author: " + aBook.getAuthor());
         }
         //cetak semua buku
         for(Map.Entry b: books.entrySet()){
-            Buku buku=(Buku) b.getValue();
+            X_Buku buku=(X_Buku) b.getValue();
             System.out.println(b.getKey()+": "+buku.getTitle());
         }
     }
-}*/
+}
